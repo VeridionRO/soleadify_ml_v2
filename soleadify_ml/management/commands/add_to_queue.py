aab8247ac6fc308f38c3e1ce403c404064ba991e
mihaivinaga <vinaga.mihai@gmail.com>
2019-03-04 00:37:56 +0200

    website contacts

--- soleadify_ml/management/commands/add_to_queue.py
+++ soleadify_ml/management/commands/add_to_queue.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
     help = "Add to queue"
 
     def handle(self, *args, **options):
-        categories = CategoryWebsite.objects.values('website_id').filter(category_id__in=[10506, 10506]).all()
+        categories = CategoryWebsite.objects.values('website_id').filter(category_id__in=[10506, 10511]).all()
         progress_bar = tqdm(desc="Processing", total=len(categories))
         for category in categories:
             scrapping.delay(category['website_id'])
