0d27efb439af974cc02137a481d023140f5eef34
mihaivinaga <vinaga.mihai@gmail.com>
2019-04-08 19:48:02 +0300

    website contacts

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -26,7 +26,7 @@ USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 1
+CONCURRENT_REQUESTS = 32
 REACTOR_THREADPOOL_MAXSIZE = 20
 TELNETCONSOLE_PORT = None
 DOWNLOAD_TIMEOUT = 30
@@ -36,7 +36,7 @@ SPLASH_URL = 'http://localhost:8050/'
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 5
+# DOWNLOAD_DELAY = 5
 # The download delay setting will honor only one of:
 CONCURRENT_REQUESTS_PER_DOMAIN = 8
 # CONCURRENT_REQUESTS_PER_IP = 16
